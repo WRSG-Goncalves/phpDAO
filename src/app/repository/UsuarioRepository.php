7afe2e838d0c55eb853db8a715565c5f60452b9e
Wilson Gonçalves <wilssp@gmail.com>
2024-09-19 22:31:55 -0300

    Repositório de usuarios

---
+++ src/app/repository/UsuarioRepository.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\repositories;
+
+use App\entities\Usuario;
+use PDO;
+
+class UsuarioRepository
+{
+    private PDO $connection;
+
+    public function __construct(PDO $connection)
+    {
+        $this->connection = $connection;
+    }
+
+    public function findById(int $id): ?Usuario
+    {
+        $stmt = $this->connection->prepare("SELECT * FROM usuarios WHERE id = :id");
+        $stmt->bindValue(':id', $id, PDO::PARAM_INT);
+        $stmt->execute();
+
+        $data = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        return $data ? $this->mapToEntity($data) : null;
+    }
+
+    public function listAll(): array
+    {
+        $stmt = $this->connection->query("SELECT * FROM usuarios");
+        $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+        return array_map([$this, 'mapToEntity'], $data);
+    }
+
+    private function mapToEntity(array $data): Usuario
+    {
+        $usuario = new Usuario($data['nome'], $data['email'], '');
+        //$usuario->setId($data['id']);
+        // Aqui, se você deseja também definir a senha ou outras propriedades, faça conforme necessário.
+        return $usuario;
+    }
+}
